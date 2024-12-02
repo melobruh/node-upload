d96356619307512cd5695c0cfadbf7ae6f8e36dc
Galia <aaron.carmelo.galia@accenture.com>
2024-12-02 11:48:51 +0800

    Added: uploads folder checking, text and csv file type checking

--- index.js
+++ index.js
@@ -3,19 +3,11 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 const path = require('path');
 const File = require('./models/File');  // Import the file model
+const fs = require('fs');
 
 const app = express();
 
-// Connect to MongoDB
-// mongoose.connect('mongodb://localhost:27017/fileUpload', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// }).then(() => {
-//   console.log('Connected to MongoDB');
-// }).catch((err) => {
-//   console.error('Error connecting to MongoDB:', err);
-// });
-
+//connection to mongo
 const connect = async () => {
   try {
     await mongoose.connect('mongodb://localhost:27017/fileUpload', {
@@ -30,6 +22,11 @@ const connect = async () => {
 
 connect();
 
+// Check if uploads folder exists, create one
+if (!fs.existsSync('uploads')) {
+  fs.mkdirSync('uploads');
+}
+
 // Set up Multer for file storage configuration
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -40,32 +37,36 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage });
+//filter text/csv file type
+const fileFilter = (req, file, cb) => {
+  file.mimetype === 'text/plain' || file.mimetype === 'text/csv' ? cb(null, true) : cb(null, false);
+}
+
+const upload = multer({ fileFilter, storage });
 
 // Route to upload a single file
 app.post('/upload', upload.single('file'), async (req, res) => {
   try {
     if (!req.file) {
-      return res.status(400).send('No file uploaded');
+      return res.status(400).send('Error uploading file/Unsupported file type');
     }
+      // Save file metadata to MongoDB
+      const newFile = new File({
+        filename: req.file.filename,
+        path: req.file.path,
+        originalName: req.file.originalname,
+        size: req.file.size,
+      });
 
-    // Save file metadata to MongoDB
-    const newFile = new File({
-      filename: req.file.filename,
-      path: req.file.path,
-      originalName: req.file.originalname,
-      size: req.file.size,
-    });
+      await newFile.save();
 
-    await newFile.save();
-
-    res.status(200).send('File uploaded successfully');
+      res.status(200).send('File uploaded successfully');
   } catch (err) {
-    res.status(500).send('Error uploading file');
+    res.status(500).send('Error uploading file/Unsupported file type');
   }
 });
 
-// Route to view uploaded files (optional)
+// get method to view the files
 app.get('/files', async (req, res) => {
   try {
     const files = await File.find();
@@ -75,7 +76,7 @@ app.get('/files', async (req, res) => {
   }
 });
 
-// Start the server
+// server start
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}/`);
